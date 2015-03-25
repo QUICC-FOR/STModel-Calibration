61e468ef326e4bf1fe56e96aff89a2378eb3b4f6
iboulangeat <isabelle.boulangeat@gmail.com>
2015-03-25 14:31:18 -0400

    update pars from glm

--- scripts/7-cross_valid.r
+++ scripts/7-cross_valid.r
@@ -2,7 +2,7 @@ rm(list = ls())
 
 #sdm = "rf"
 sdm = "multinom"
-propData = 0.25
+propData = 0.33
 
 #--
 veget_pars = read.table(paste("../estimated_params/GenSA_initForFit_", sdm, "_",propData, ".txt", sep=""))
