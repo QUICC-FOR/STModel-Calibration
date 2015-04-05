4c5f81aa87a6e3825fcd2fdafea2e89cf899faa3
iboulangeat <isabelle.boulangeat@gmail.com>
2015-04-05 17:46:45 -0400

    correct  gensa run repets

--- scripts/runRepetitions.sh
+++ scripts/runRepetitions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 run(){
-    Rscript 4-initForFit.R 0.33$1
-    wait
+#Rscript 4-initForFit.R 0.33$1
     Rscript 5-fit_model.R initForFit_cst_0.33$1 &
     Rscript 5-fit_model.R initForFit_rf_0.33$1 &
     Rscript 5-fit_model.R initForFit_rf_0.33$1 _less &
     Rscript 5-fit_model.R initForFit_rf_0.33$1 5y &
     Rscript 5-fit_model.R initForFit_rf_0.33$1 _less5y &
+    wait
 }
 export -f run
 parallel -j 3 run ::: 1 2 3 4 5 6 7 8 9
