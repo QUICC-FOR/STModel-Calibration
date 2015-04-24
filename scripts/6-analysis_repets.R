3dfb4fac12460f0979b61a3d2ad8a08ddc20610e
iboulangeat <isabelle.boulangeat@gmail.com>
2015-04-24 14:46:37 -0400

    estimation april - long run

--- scripts/6-analysis_repets.R
+++ scripts/6-analysis_repets.R
@@ -1,60 +1,55 @@
 rm(list = ls())
 
 sdm = "rf"
-sdm = "multinom"
-propData = 0.3
+propData = 0.33
+ordre = 3
+step = 5
+
+(name = paste(sdm,"_", propData, "_", ordre, "_",step, "y",sep=""))
+
 
 #--
 veget_pars = data.frame(matrix(NA, ncol = 9, nrow = 49))
+logll = rep(0, 9)
 for( i in 1:9)
 {
-veget_pars[,i] = as.vector(read.table(paste("../estimated_params/GenSA_initForFit_", sdm, "_",propData, i,".txt", sep="")))
-}
-rownames(veget_pars) = rownames(as.vector(read.table(paste("../estimated_params/GenSA_initForFit_", sdm, "_",propData, i,".txt", sep=""))))
-veget_pars
+estimatedPars = read.table(paste("../estimated_params/GenSA_", sdm, "_", propData, i, "_", ordre, "_",step, "y.txt", sep=""))
+parnames = estimatedPars[,1]
+veget_pars[,i] = as.vector(estimatedPars[,2])
 
+load(paste("../estimated_params/GenSA_", sdm, "_", propData, i, "_", ordre, "_",step, "y.RData", sep=""))
+logll[i]= estim.pars$value
+}
+rownames(veget_pars) = parnames
 ##-------
 
 apply(veget_pars, 1, mean)
 apply(veget_pars, 1, sd)
-veget_pars[c("ab2","at0"),]
+logll
+which.min(logll)
 
 #--
-
-tpseq=seq(-4,2,l=100)
-ppseq=seq(-2,5,l=100)
 load("scale_info.Robj")
+## Temp -4 à 10
+## PP 700 à 1300
+Trange = c(-4, 10)
+Tticks = max(Trange)-min(Trange)+1
+PPrange = c(700, 1300)
+Tbounds = (Trange - vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"]
+PPbounds = (PPrange - vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"]
+PPticks = (max(PPrange)-min(PPrange))/100 +1
+
+tpseq=seq(Tbounds[1],Tbounds[2],l=100)
+ppseq=seq(PPbounds[1],PPbounds[2],l=100)
 
 scaled.axis<- function(){
 temp = tpseq*vars.sd["annual_mean_temp"]+vars.means["annual_mean_temp"]
 precip = ppseq*vars.sd["tot_annual_pp"]+vars.means["tot_annual_pp"]
-axis(1, at = seq((round(min(temp))-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], (round(max(temp))-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], l=18), labels = seq(round(min(temp)), round(max(temp)), l = 18))
-axis(2, at = seq((700-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], (1800-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], l=12), labels = seq(700, 1800, l = 12))
+axis(1, at = seq(Tbounds[1], Tbounds[2], l=Tticks), labels = seq(Trange[1], Trange[2], l = Tticks))
+axis(2, at = seq(PPbounds[1],PPbounds[2], l=PPticks), labels = seq(PPrange[1], PPrange[2], l = PPticks))
 }
 
 #---
-
-
-
-#--
-load(paste("../estimated_params/GenSA_initForFit_", sdm, "_", propData, "1.RData", sep = ""))
-estim.pars$value
-#-----
-# diagnostic convergence
-#----
-#jpeg(paste("../figures/diagnostic",fit,".jpeg", sep=""), height=5000, width=5000, res=600)
-par(mfrow = c(2,2), mar = c(4, 4, 2,1))
-mat = data.frame(estim.pars$trace.mat)
-mat$number = 1:nrow(mat)
-plot(nb.steps~number, data = mat, xlab = "anneal time", ylab = "nb.steps", cex=.2, type = "l")
-plot(temperature~number, data = mat, cex=.2, type = "l")
-plot(function.value~number, data = mat, cex=.2, type = "l")
-plot(current.minimum~number, data = mat, cex = .2, type="l")
-#dev.off()
-#-----
-# check estimated params and bounds
-#----
-#load(paste("initForFit_",sdm, "_", propData, "1.RData", sep = ""))
 
 #------
 # look at transition probabilities
@@ -64,15 +59,15 @@ ENV = expand.grid(TP =tpseq , PP = ppseq)
 ENV1 = ENV$TP
 ENV2 = ENV$PP
 
-e7 = 0
-params=veget_pars[,9]
-names(params) = rownames(veget_pars)
 
+calcMacroPars = function(params, ENV)
+{
+names(params) = rownames(veget_pars)
     logit_alphab 	= params["ab0"] + params["ab1"]*ENV1 + params["ab2"]*ENV2 + params["ab3"]*ENV1^2 + params["ab4"]*ENV2^2 + params["ab5"]*ENV1^3 + params["ab6"]*ENV2^3
     logit_alphat 	= params["at0"] + params["at1"]*ENV1 + params["at2"]*ENV2 + params["at3"]*ENV1^2 + params["at4"]*ENV2^2 + params["at5"]*ENV1^3 + params["at6"]*ENV2^3
     logit_betab 	= params["bb0"] + params["bb1"]*ENV1 + params["bb2"]*ENV2 + params["bb3"]*ENV1^2 + params["bb4"]*ENV2^2 + params["bb5"]*ENV1^3 + params["bb6"]*ENV2^3
     logit_betat 	= params["bt0"] + params["bt1"]*ENV1 + params["bt2"]*ENV2 + params["bt3"]*ENV1^2 + params["bt4"]*ENV2^2 + params["bt5"]*ENV1^3 + params["bt6"]*ENV2^3
-    logit_theta	= params["t0"] + params["t1"]*ENV1 + params["t2"]*ENV2 + params["t3"]*ENV1^2 + params["t4"]*ENV2^2 + params["t5"]*ENV1^3 + params["t6"]*ENV2^3
+    logit_theta	= params["th0"] + params["th1"]*ENV1 + params["th2"]*ENV2 + params["th3"]*ENV1^2 + params["th4"]*ENV2^2 + params["th5"]*ENV1^3 + params["th6"]*ENV2^3
     logit_thetat	= params["tt0"] + params["tt1"]*ENV1 + params["tt2"]*ENV2 + params["tt3"]*ENV1^2 + params["tt4"]*ENV2^2 + params["tt5"]*ENV1^3 + params["tt6"]*ENV2^3
     logit_eps 	= params["e0"]  + params["e1"]*ENV1 + params["e2"]*ENV2  + params["e3"]*ENV1^2 + params["e4"]*ENV2^2 + params["e5"]*ENV1^3 + params["e6"]*ENV2^3 
     #e7*EB
@@ -82,7 +77,7 @@ names(params) = rownames(veget_pars)
     expx = ifelse(exp(x)==Inf, .Machine$double.xmax, exp(x))
     expx/(1+expx)
     }
-    
+ 
 macroPars = data.frame(alphab = logit_reverse(logit_alphab), 
 alphat = logit_reverse(logit_alphat),
 betab = logit_reverse(logit_betab),
@@ -91,7 +86,15 @@ theta = logit_reverse(logit_theta),
 thetat = logit_reverse(logit_thetat),
 eps = logit_reverse(logit_eps))
 
-summary(macroPars)
+return(macroPars)
+}
+
+macroPars_tab= apply(veget_pars, 2, calcMacroPars, ENV=ENV)
+macroPars_tab = array(unlist(macroPars_tab), dim = c(nrow(macroPars_tab[[1]]), ncol(macroPars_tab[[1]]), length(macroPars_tab)))
+
+macroPars = data.frame(apply(macroPars_tab, c(1,2), mean))
+head(macroPars)
+colnames(macroPars) = c("alphab", "alphat", "betab", "betat", "theta", "thetat", "eps")
 #---
 #
 pal = colorRampPalette(c("lightblue", "yellow", "orange"), space = "rgb")
@@ -112,33 +115,47 @@ scaled.axis()
 #------
 # probabilites transition (no neighborhood restriction)
 #------
+head(ENV)
+colnames(ENV) = c("annual_mean_temp", "tot_annual_pp")
+ENV$mean_diurnal_range = rep(0, nrow(ENV))
+ENV$pp_warmest_quarter = rep(0, nrow(ENV))
+ENV$pp_wettest_period = rep(0, nrow(ENV))
+ENV$mean_temp_wettest_quarter = rep(0, nrow(ENV))
+ENV$mean_temp_driest_quarter = rep(0, nrow(ENV))
 
-pTransitions = data.frame(pRT = macroPars$alphat*0.5*(1-macroPars$alphab*0.5),
-pRB = macroPars$alphab*0.5*(1-macroPars$alphat*0.5),
-pRM = macroPars$alphat*0.5*macroPars$alphab*0.5,
-pMT = macroPars$theta*macroPars$thetat*(1-macroPars$eps),
-pMB = macroPars$theta*(1-macroPars$thetat)*(1-macroPars$eps),
-pTM = macroPars$betab*0.5*(1-macroPars$eps),
-pBM = macroPars$betat*0.5*(1-macroPars$eps),
+load("../data/RandomForest_complete.rObj")
+library(randomForest)
+set.seed(rs)
+proj2 = predict(SDM2,new=ENV,"prob", OOB=TRUE)
+
+nB = proj2[,"B"]
+nT = proj2[,"T"]
+nM = proj2[,"M"]
+
+pTransitions = data.frame(pRT = macroPars$alphat*(nT+nM)*(1-macroPars$alphab*(nB+nM)),
+pRB = macroPars$alphab*(nB+nM)*(1-macroPars$alphat*(nT+nM)),
+pRM = macroPars$alphat*(nT+nM)*macroPars$alphab*(nB+nM),
+pMT = macroPars$theta*macroPars$thetat,
+pMB = macroPars$theta*(1-macroPars$thetat),
+pTM = macroPars$betab,
+pBM = macroPars$betat,
 eps = macroPars$eps)
 
-jpeg(paste("../figures/estim_transitions_", sdm, "_03x9.jpeg", sep=""), height=3000, width=5000, res=600)
+summary(pTransitions)
 
 par(mfrow = c(2,4), mar = c(4,4,1,1), cex=0.8)
 
 for (i in 1:ncol(pTransitions))
 {
 image(x=tpseq, y=ppseq, z = matrix(pTransitions[,i], ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = pal(12), main = colnames(pTransitions)[i], xaxt = "n", yaxat="n")
-contour(x=tpseq, y=ppseq, z = matrix(pTransitions[,i], ncol = length(ppseq), nrow = length(tpseq)), add=TRUE)
+#contour(x=tpseq, y=ppseq, z = matrix(pTransitions[,i], ncol = length(ppseq), nrow = length(tpseq)), add=TRUE)
 scaled.axis()
 }
-dev.off()
 
 ### 
 #------
-# invasibility
-#------
-
+# equilibrium analysis
+##--------------------
 pars= data.frame(
 alphaB = macroPars$alphab,
 alphaT = macroPars$alphat,
@@ -164,6 +181,42 @@ return(list(c(dT, dB, dM)))
 library(rootSolve)
 
 
+eq.winner = function(pars, model)
+{
+fparams = pars
+init = c(T = 0.33, B = 0.33, M=0.33)
+(eq = stode(y = init, func = model, parms = fparams, positive = TRUE)$y)
+return(names(which.max(eq)))
+}
+
+eq = t(apply(pars, 1, eq.winner, model =model))
+eq = as.factor(eq)
+##-----
+#jpeg(paste("../figures/equilibrium_map_", sdm, "_",propData, option, ".jpeg", sep=""), height=3000, width=3000, res = 300)
+#
+colo = c(M = "orange", B = "darkgreen", T = "lightgreen")
+
+layout(matrix(c(1,2),nr=2,nc=1,byrow=TRUE),heights = c(1,6))
+
+par(mar=c(0,0,0,0))
+plot(1, type = "n", axes=FALSE, xlab="", ylab="")
+title("",cex=2)
+legend("center",legend = levels(eq),fill = colo[levels(eq)],bty = "n", cex = 0.8, ncol =2)
+par(mar=c(5,5,0,2))
+
+image(x=tpseq, y=ppseq, z = matrix(as.numeric(eq), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Annual mean temperature (°C)", ylab = "Annual precipitations (mm)", col = colo[levels(eq)], main = "", xaxt = "n", yaxt="n")
+scaled.axis()
+#
+#dev.off()
+
+
+### 
+#------
+# invasibility
+#------
+
+library(rootSolve)
+
 eq.eigen = function(pars, model, eqState = "B")
 {
 fparams = pars
@@ -174,76 +227,53 @@ if(eqState=="T") init = c(T = as.numeric(1-fparams["eps"]/fparams["alphaT"]), B=
 if(is.infinite(sum(init))) init = c(T=0, B = 0, M=0)
 (eq = stode(y = init, func = model, parms = fparams, positive = TRUE)$y)
 (jacob = jacobian.full(y = eq, func= model, parms=fparams))
+#print(jacob)
 return(eigen(jacob)$values)
 }
 
-invT = t(apply(pars, 1, eq.eigen, model =model, eqState="B"))
-invT.class = ifelse(invT[,3]<0, "stable", "invadeM")
-invT.class = ifelse(invT[,1]>0, "invadeT", invT.class)
-invT.class = as.factor(invT.class)
-T.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaT"]})
-invT.class[T.growth<0] = "stable"
+ 
+ invT = t(apply(pars, 1, eq.eigen, model =model, eqState="B"))
+ invT.class = ifelse(invT[,3]<0, "stable", "invadeM")
+ invT.class = ifelse(invT[,1]>0, "invadeT", invT.class)
+ T.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaT"]})
+ invT.class[T.growth<0] = "stable"
+ invT.class = as.factor(invT.class)
 
-invB = t(apply(pars, 1, eq.eigen, model =model, eqState="T"))
-invB.class = ifelse(invB[,3]<0, "stable", "invadeM")
-invB.class = ifelse(invB[,2]>0, "invadeB", invB.class)
-invB.class = as.factor(invB.class)
-B.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaB"]})
-invB.class[B.growth<0] = "stable"
+ invB = t(apply(pars, 1, eq.eigen, model =model, eqState="T"))
+ invB.class = ifelse(as.numeric(invB[,3])<0, "stable", "invadeM")
+ invB.class = ifelse(as.numeric(invB[,2])>0, "invadeB", invB.class)
+ B.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaB"]})
+ invB.class[B.growth<0] = "stable"
+ invB.class = as.factor(invB.class)
 
 ##--
 coexist = numeric(length(invT))
 coexist[invT.class=="stable" & invB.class=="stable"] = "AltSS"
-coexist[invT.class=="stable" & invB.class=="invadeB"] = "B wins"
-coexist[invT.class=="stable" & invB.class=="invadeM"] = "B dominates (with M)"
+coexist[invT.class=="stable" & invB.class=="invadeB"] = "B/M invade"
+coexist[invT.class=="stable" & invB.class=="invadeM"] = "B/M invade"
 
-coexist[invT.class=="invadeM" & invB.class=="invadeM"] = "M wins (coexistence)"
-coexist[invT.class=="invadeM" & invB.class=="invadeB"] = "B dominates (with M)"
-coexist[invT.class=="invadeM" & invB.class=="stable"] = "T dominates (with M)"
+coexist[invT.class=="invadeM" & invB.class=="invadeM"] = "reciprocal invasion"
+coexist[invT.class=="invadeM" & invB.class=="invadeB"] = "reciprocal invasion"
+coexist[invT.class=="invadeM" & invB.class=="stable"] = "T/M invade"
 
-coexist[invT.class=="invadeT" & invB.class=="stable"] = "T wins"
-coexist[invT.class=="invadeT" & invB.class=="invadeM"] = "T dominates (with M)"
-coexist[invT.class=="invadeT" & invB.class=="invadeB"] = "??"
+coexist[invT.class=="invadeT" & invB.class=="stable"] = "T/M invade"
+coexist[invT.class=="invadeT" & invB.class=="invadeM"] = "reciprocal invasion"
+coexist[invT.class=="invadeT" & invB.class=="invadeB"] = "reciprocal invasion"
 table(coexist)
 coexist = as.factor(coexist)
 levels(coexist)
 ##---
-par(mfrow = c(1,2), mar = c(4,4,1,1), cex=0.8)
+colo = c(AltSS = "pink", 'reciprocal invasion' = "lightblue", 'B/M invade' = "darkgreen", 'T/M invade' = "lightgreen", '??' = 1)
 
-image(x=tpseq, y=ppseq, z = matrix(as.numeric(invT.class), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = c("darkgreen","lightgreen", "grey"), main = "invasibility of T (when B every where)", xaxt = "n", yaxt="n")
-scaled.axis()
-legend("topleft",legend = levels(invT.class),fill = c("darkgreen","lightgreen", "grey"),bty = "n", cex = 0.8, ncol =1)
-
-image(x=tpseq, y=ppseq, z = matrix(as.numeric(invB.class), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = c("darkgreen","lightgreen", "grey"), main = "invasibility of B (when T everywhere)", xaxt = "n", yaxt="n")
-scaled.axis()
-legend("topleft",legend = levels(invB.class),fill = c("darkgreen","lightgreen", "grey"),bty = "n", cex = 0.8, ncol =1)
-
-
-##-----
-jpeg(paste("../figures/equilibrium_map_", sdm, "_03x9.jpeg", sep=""), height=3000, width=3000, res = 300)
-colo = c("pink","darkorange", "darkgreen", "orange3", "orange", "lightgreen")
 layout(matrix(c(1,2),nr=2,nc=1,byrow=TRUE),heights = c(1,6))
 
 par(mar=c(0,0,0,0))
 plot(1, type = "n", axes=FALSE, xlab="", ylab="")
 title("",cex=2)
-legend("center",legend = levels(coexist),fill = colo,bty = "n", cex = 0.8, ncol =2)
+legend("center",legend = levels(coexist),fill = colo[levels(coexist)],bty = "n", cex = 0.8, ncol =2)
 par(mar=c(5,5,0,2))
 
-image(x=tpseq, y=ppseq, z = matrix(as.numeric(coexist), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Annual mean temperature (°C)", ylab = "Annual precipitations (mm)", col = colo, main = "invasibility of B (when T everywhere)", xaxt = "n", yaxt="n")
+image(x=tpseq, y=ppseq, z = matrix(as.numeric(coexist), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Annual mean temperature (°C)", ylab = "Annual precipitations (mm)", col = colo[levels(coexist)], main = "", xaxt = "n", yaxt="n")
 scaled.axis()
-dev.off()
-
-
-
-
-
-#reactivite -> potentiel à amplifier les fluctuations env.
-#M = (jacob + t(jacob))/2
-#max(abs(eigen(M)$values))
-#M.veg = (jacob.veg + t(jacob.veg))/2
-#max(abs(eigen(M.veg)$values))
 #
-
-
-
+#dev.off()
--- scripts/6-fastAnalysis.R
+++ scripts/6-fastAnalysis.R
@@ -1,32 +1,37 @@
 rm(list = ls())
 
 sdm = "rf"
-sdm = "cst"
-propData = 0.337
-option = "_less"
-option = "5y"
-option = "_less5y"
-option = ""
-option = "2"
-option = "_less5y2"
+#sdm = "cst"
+propData = 0.338
+ordre = 3
+step = 5
+
+(name = paste(sdm,"_", propData, "_", ordre, "_",step, "y",sep=""))
 
 #--
-veget_pars = read.table(paste("../estimated_params/GenSA_initForFit_", sdm, "_",propData, option, ".txt", sep=""))
+veget_pars = read.table(paste("../estimated_params_avr2015/GenSA_", name, ".txt", sep=""))
 #load(paste("initForFit_",sdm, "_", propData, ".RData", sep = ""))
-load(paste("../estimated_params/GenSA_initForFit_", sdm, "_", propData, option, ".RData", sep = ""))
-mat = read.table(paste("../estimated_params/traceMat_initForFit_", sdm, "_", propData, option, ".trMat", sep=""), h=T)
+load(paste("../estimated_params_avr2015/GenSA_", name, ".RData", sep = ""))
+mat = read.table(paste("../estimated_params_avr2015/traceMat_", name, ".trMat", sep=""), h=T)
 #--
-print(fit)
-#--
-tpseq=seq(-4,2,l=100)
-ppseq=seq(-2,5,l=100)
 load("scale_info.Robj")
+## Temp -4 à 10
+## PP 700 à 1300
+Trange = c(-4, 10)
+Tticks = max(Trange)-min(Trange)+1
+PPrange = c(700, 1300)
+Tbounds = (Trange - vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"]
+PPbounds = (PPrange - vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"]
+PPticks = (max(PPrange)-min(PPrange))/100 +1
+
+tpseq=seq(Tbounds[1],Tbounds[2],l=100)
+ppseq=seq(PPbounds[1],PPbounds[2],l=100)
 
 scaled.axis<- function(){
 temp = tpseq*vars.sd["annual_mean_temp"]+vars.means["annual_mean_temp"]
 precip = ppseq*vars.sd["tot_annual_pp"]+vars.means["tot_annual_pp"]
-axis(1, at = seq((round(min(temp))-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], (round(max(temp))-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], l=18), labels = seq(round(min(temp)), round(max(temp)), l = 18))
-axis(2, at = seq((700-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], (1800-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], l=12), labels = seq(700, 1800, l = 12))
+axis(1, at = seq(Tbounds[1], Tbounds[2], l=Tticks), labels = seq(Trange[1], Trange[2], l = Tticks))
+axis(2, at = seq(PPbounds[1],PPbounds[2], l=PPticks), labels = seq(PPrange[1], PPrange[2], l = PPticks))
 }
 
 #---
@@ -49,6 +54,7 @@ plot(current.minimum~number, data = mat, cex = .2, type="l")
 # check estimated params and bounds
 #----
 parsBounds = data.frame(hi=unlist(par_hi),lo= unlist(par_lo))
+rownames(parsBounds) = names(pars)
 parsBounds$estim = rep(0, nrow(parsBounds))
 parsBounds[names(pars), "estim"] = pars
 par(mfrow = c(1,1), mar = c(2, 2, 2,1))
@@ -57,8 +63,7 @@ points(parsBounds$lo, col=1, pch=15, cex=.5)
 points(parsBounds$estim, col=2, pch=19, cex=.5)
 title("estimated params and given bounds")
 
-pp = parsBounds
-plot(parsBounds$estim, pp$estim)
+
 #------
 # look at transition probabilities
 #------
@@ -67,9 +72,18 @@ ENV = expand.grid(TP =tpseq , PP = ppseq)
 ENV1 = ENV$TP
 ENV2 = ENV$PP
 
-e7 = 0
-params=parsBounds$estim
-names(params) = rownames(parsBounds)
+params=rep(0, 49)
+names(params) = c("ab0", "ab1", "ab2", "ab3","ab4","ab5","ab6",
+"at0", "at1" , "at2", "at3", "at4", "at5", "at6", 
+"bb0", "bb1", "bb2", "bb3", "bb4", "bb5", "bb6",
+"bt0", "bt1", "bt2", "bt3", "bt4", "bt5", "bt6",
+"tt0", "tt1", "tt2", "tt3", "tt4", "tt5", "tt6", 
+"th0", "th1", "th2", "th3", "th4", "th5", "th6", 
+"e0", "e1", "e2", "e3", "e4", "e5", "e6")
+
+params[names(pars)] = pars
+
+print(params)
 
     logit_alphab 	= params["ab0"] + params["ab1"]*ENV1 + params["ab2"]*ENV2 + params["ab3"]*ENV1^2 + params["ab4"]*ENV2^2 + params["ab5"]*ENV1^3 + params["ab6"]*ENV2^3
     logit_alphat 	= params["at0"] + params["at1"]*ENV1 + params["at2"]*ENV2 + params["at3"]*ENV1^2 + params["at4"]*ENV2^2 + params["at5"]*ENV1^3 + params["at6"]*ENV2^3
@@ -131,9 +145,9 @@ library(randomForest)
 set.seed(rs)
 proj2 = predict(SDM2,new=ENV,"prob", OOB=TRUE)
 
-nB = proj2["B"]
-nT = proj2["T"]
-nM = proj2["M"]
+nB = proj2[,"B"]
+nT = proj2[,"T"]
+nM = proj2[,"M"]
 
 pTransitions = data.frame(pRT = macroPars$alphat*(nT+nM)*(1-macroPars$alphab*(nB+nM)),
 pRB = macroPars$alphab*(nB+nM)*(1-macroPars$alphat*(nT+nM)),
@@ -151,7 +165,7 @@ par(mfrow = c(2,4), mar = c(4,4,1,1), cex=0.8)
 for (i in 1:ncol(pTransitions))
 {
 image(x=tpseq, y=ppseq, z = matrix(pTransitions[,i], ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = pal(12), main = colnames(pTransitions)[i], xaxt = "n", yaxat="n")
-contour(x=tpseq, y=ppseq, z = matrix(pTransitions[,i], ncol = length(ppseq), nrow = length(tpseq)), add=TRUE)
+#contour(x=tpseq, y=ppseq, z = matrix(pTransitions[,i], ncol = length(ppseq), nrow = length(tpseq)), add=TRUE)
 scaled.axis()
 }
 
@@ -217,7 +231,7 @@ return(list(c(dT, dB, dM)))
 # invasibility
 #------
 
-
+library(rootSolve)
 
 eq.eigen = function(pars, model, eqState = "B")
 {
@@ -234,52 +248,52 @@ return(eigen(jacob)$values)
 }
 
 invT = t(apply(pars, 1, eq.eigen, model =model, eqState="B"))
-invT.class = ifelse(invT[,3]<0, "stable", "invadeM")
-invT.class = ifelse(invT[,1]>0, "invadeT", invT.class)
-T.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaT"]})
-invT.class[T.growth<0] = "stable"
-invT.class = as.factor(invT.class)
+ invT.class = ifelse(invT[,3]<0, "stable", "invadeM")
+ invT.class = ifelse(invT[,1]>0, "invadeT", invT.class)
+ T.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaT"]})
+ invT.class[T.growth<0] = "stable"
+ invT.class = as.factor(invT.class)
 
-invB = t(apply(pars, 1, eq.eigen, model =model, eqState="T"))
-invB.class = ifelse(invB[,3]<0, "stable", "invadeM")
-invB.class = ifelse(invB[,2]>0, "invadeB", invB.class)
-B.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaB"]})
-invB.class[B.growth<0] = "stable"
-invB.class = as.factor(invB.class)
+ invB = t(apply(pars, 1, eq.eigen, model =model, eqState="T"))
+ invB.class = ifelse(as.numeric(invB[,3])<0, "stable", "invadeM")
+ invB.class = ifelse(as.numeric(invB[,2])>0, "invadeB", invB.class)
+ B.growth = apply(pars, 1, function(pars){1-pars["eps"]/pars["alphaB"]})
+ invB.class[B.growth<0] = "stable"
+ invB.class = as.factor(invB.class)
 
 ##--
 coexist = numeric(length(invT))
 coexist[invT.class=="stable" & invB.class=="stable"] = "AltSS"
-coexist[invT.class=="stable" & invB.class=="invadeB"] = "B wins"
-coexist[invT.class=="stable" & invB.class=="invadeM"] = "B dominates (with M)"
+coexist[invT.class=="stable" & invB.class=="invadeB"] = "B/M invade"
+coexist[invT.class=="stable" & invB.class=="invadeM"] = "B/M invade"
 
-coexist[invT.class=="invadeM" & invB.class=="invadeM"] = "M wins (coexistence)"
-coexist[invT.class=="invadeM" & invB.class=="invadeB"] = "B dominates (with M)"
-coexist[invT.class=="invadeM" & invB.class=="stable"] = "T dominates (with M)"
+coexist[invT.class=="invadeM" & invB.class=="invadeM"] = "reciprocal invasion"
+coexist[invT.class=="invadeM" & invB.class=="invadeB"] = "reciprocal invasion"
+coexist[invT.class=="invadeM" & invB.class=="stable"] = "T/M invade"
 
-coexist[invT.class=="invadeT" & invB.class=="stable"] = "T wins"
-coexist[invT.class=="invadeT" & invB.class=="invadeM"] = "T dominates (with M)"
-coexist[invT.class=="invadeT" & invB.class=="invadeB"] = "??"
+coexist[invT.class=="invadeT" & invB.class=="stable"] = "T/M invade"
+coexist[invT.class=="invadeT" & invB.class=="invadeM"] = "reciprocal invasion"
+coexist[invT.class=="invadeT" & invB.class=="invadeB"] = "reciprocal invasion"
 table(coexist)
 coexist = as.factor(coexist)
 levels(coexist)
 ##---
-colo = c(stable = "grey", invadeM = "blue", invadeT = "lightgreen", invadeB = "darkgreen")
-par(mfrow = c(1,2), mar = c(4,4,1,1), cex=0.8)
-
-image(x=tpseq, y=ppseq, z = matrix(as.numeric(invT.class), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = colo[levels(invT.class)], main = "invasibility of T (when B every where)", xaxt = "n", yaxt="n")
-scaled.axis()
-legend("topleft",legend = levels(invT.class),fill = colo[levels(invT.class)],bty = "n", cex = 0.8, ncol =1)
-
-image(x=tpseq, y=ppseq, z = matrix(as.numeric(invB.class), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = colo[levels(invB.class)], main = "invasibility of B (when T everywhere)", xaxt = "n", yaxt="n")
-scaled.axis()
-legend("topleft",legend = levels(invB.class),fill = colo[levels(invB.class)],bty = "n", cex = 0.8, ncol =1)
+#colo = c(stable = "grey", invadeM = "darkgreen", invadeT = "darkgreen", invadeB = "darkgreen")
+#par(mfrow = c(1,2), mar = c(4,4,1,1), cex=0.8)
+#
+#image(x=tpseq, y=ppseq, z = matrix(as.numeric(invT.class), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = colo[levels(invT.class)], main = "invasibility of T (when B every where)", xaxt = "n", yaxt="n")
+#scaled.axis()
+#legend("topleft",legend = levels(invT.class),fill = colo[levels(invT.class)],bty = "n", cex = 0.8, ncol =1)
+#
+#image(x=tpseq, y=ppseq, z = matrix(as.numeric(invB.class), ncol = length(ppseq), nrow = length(tpseq)),xlab = "Temperature", ylab = "Precipitations", col = colo[levels(invB.class)], main = "invasibility of B (when T everywhere)", xaxt = "n", yaxt="n")
+#scaled.axis()
+#legend("topleft",legend = levels(invB.class),fill = colo[levels(invB.class)],bty = "n", cex = 0.8, ncol =1)
 
 
 ##-----
 #jpeg(paste("../figures/equilibrium_map_", sdm, "_",propData, option, ".jpeg", sep=""), height=3000, width=3000, res = 300)
 #
-colo = c(AltSS = "pink", 'M wins (coexistence)' = "lightblue", 'B wins' = "darkgreen", 'B dominates (with M)' ="grey30", 'T dominates (with M)' = "grey70", 'T wins' = "lightgreen", '??' = 1)
+colo = c(AltSS = "pink", 'reciprocal invasion' = "lightblue", 'B/M invade' = "darkgreen", 'T/M invade' = "lightgreen", '??' = 1)
 
 layout(matrix(c(1,2),nr=2,nc=1,byrow=TRUE),heights = c(1,6))
 
@@ -293,5 +307,3 @@ image(x=tpseq, y=ppseq, z = matrix(as.numeric(coexist), ncol = length(ppseq), nr
 scaled.axis()
 #
 #dev.off()
-
-estim.pars$value
