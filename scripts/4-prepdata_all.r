cfc9bdd740a489bc7f39fefe305df31ba1aa6a8e
iboulangeat <isabelle.boulangeat@gmail.com>
2015-06-23 18:07:59 -0400

    update prep data all with treated NA

--- scripts/4-prepdata_all.r
+++ scripts/4-prepdata_all.r
@@ -2,52 +2,68 @@ rm(list=ls())
 ##-----------
 ## load dat
 ##-----------
-load("../data/transitions_r1.RData")
-dataProj = transitionData
-head(dataProj)
-dim(dataProj)
-# subset 10 degree
-select = unique(dataProj$plot[which(dataProj$annual_mean_temp<=10)])
-dataProj_subset10 = dataProj[dataProj$plot %in% select,]
-# rescale
+
+dat = read.table("../data/projection_rf_complete_woLatLon.txt", h=T)
 load("scale_info.Robj")
-dat_scale = dataProj_subset10[c("annual_mean_temp", "tot_annual_pp")]
-dat_scale = t(apply(dat_scale, 1, function(x) {(x-vars.means[c("annual_mean_temp", "tot_annual_pp")])/vars.sd[c("annual_mean_temp", "tot_annual_pp")]}))
-dat_scale = data.frame(dat_scale)
-#head(dat_scale)
-dim(dat_scale)
-# remove transitions directes B->T ou T->B
-trBT = c(which(dataProj_subset10$state1 == "T" & dataProj_subset10$state2 == "B"), which(dataProj_subset10$state1 == "B" & dataProj_subset10$state2 == "T"))
+head(dat)
+# subset 10 degree
+dat = dat[which(dat$annual_mean_temp<=((10-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"])),]
+#remove transitions directes B->T ou T->B
+trBT = c(which(dat$state1 == "T" & dat$state2 == "B"), which(dat$state1 == "B" & dat$state2 == "T"))
 # clean transition time
-dataProj_subset10$itime = dataProj_subset10$year2 - dataProj_subset10$year1
-rmItime= c(which(dataProj_subset10$itime<5), which(dataProj_subset10$itime>15))
+dat$itime = dat$year2 - dat$year1
+rmItime= c(which(dat$itime<5), which(dat$itime>15))
 # clean dataset
 toremove = unique(c(trBT, rmItime))
-dataProj_subset10 = dataProj_subset10[-toremove,]
-# create dataset
-dat = dat_scale[-toremove,]
-#rename variables
-colnames(dat) = c("ENV1", "ENV2")
-dat$st0 = dataProj_subset10[,"state1"]
-dat$st1 = dataProj_subset10[,"state2"]
-dat$itime = dataProj_subset10[,"itime"]
-dat$plot_id = dataProj_subset10[,"plot"]
-# neighborhood
-pred = read.table("../data/projection_rf_complete.txt", h=T)
-pred = pred[pred$plot %in% select,]
-pred = pred[-toremove,]
-dat$EB = pred$B
-dat$ET = pred$T
-dat$EM = pred$M 
-head(dat)
+dat = dat[-toremove,]
+dim(dat)
+summary(dat)
+## attention NA (from soil missing data)
 
-save(dat, file = "datAll.RData")
+#rename variables
+dat$ENV1 = dat[,"annual_mean_temp"]
+dat$ENV2 = dat[,"tot_annual_pp"]
+dat$st0 = dat[,"state1"]
+dat$st1 = dat[,"state2"]
+dat$plot_id = dat[,"plot"]
+
+dat = dat[,c("plot_id", "ENV1","ENV2","st0","st1","itime","B","M","R","T","year1","year2","annual_mean_temp",   "tot_annual_pp" ,     "mean_diurnal_range", "ph_2cm"        ,     "slp"            ,    "lat"       ,        "lon")]
+
+head(dat)
+dim(dat)
+
+dat = na.omit(dat)
+dim(dat)
+
+save(dat, file = "datAll_woLatLon.RData")
+
+#-----
+Temp.lim = c((-4-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], (10-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"])
+Temp.ax = function(x)
+{
+temp = dat[,"annual_mean_temp"]*vars.sd["annual_mean_temp"]+vars.means["annual_mean_temp"]
+axis(1, at = seq((round(min(temp))-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], (round(max(temp))-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], l=16), labels = seq(round(min(temp)), round(max(temp)), l = 16))
+}
+#-
+pp.lim = c((500-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], (2000-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"])
+pp.ax = function(x)
+{
+pp = dat[,"tot_annual_pp"]*vars.sd["tot_annual_pp"]+vars.means["tot_annual_pp"]
+axis(2, at = seq((round(min(pp))-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], (round(max(pp))-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], l=16), labels = seq(round(min(pp)), round(max(pp)), l = 16))
+}
 
 #
 jpeg("../figures/calibration_data_in_climSpace.jpeg", height=3000, width=5000, res=600)
 
-plot(dataProj_subset10$annual_mean_temp, dataProj_subset10$tot_annual_pp, ylab = "precipitations", xlab = "temperature" , cex =.2, pch = 19)
-rect(xleft=-4, xright=10, ybottom=700, ytop=1300, col=NA, border = 2, lwd =1.2)
+plot(dat$annual_mean_temp , dat$tot_annual_pp, ylab = "precipitations", xlab = "temperature" ,cex = .2, pch = 20, xaxt="n", yaxt="n", xlim = Temp.lim, ylim = pp.lim, col = 1)
+Temp.ax()
+pp.ax()
+
+rect(xleft=(-4-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], 
+xright=(10-vars.means["annual_mean_temp"])/vars.sd["annual_mean_temp"], 
+ybottom=(700-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"], 
+ytop=(1300-vars.means["tot_annual_pp"])/vars.sd["tot_annual_pp"],  
+col=NA, border = 2, lwd =1.2)
 
 
 dev.off()
