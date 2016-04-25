f8814dffbe5a373af03f074291f01b33981b7050
Matt Talluto <mtalluto@gmail.com>
2016-04-25 14:23:19 +0200

    New mp2 script

--- mcmc_4state/prep_mcmc.r
+++ mcmc_4state/prep_mcmc.r
@@ -1,19 +1,38 @@
+#!/usr/bin/env Rscript
+
 # prep_mcmc.r
 # March 1 2016
 # Matt Talluto
 #
-# Prepare 4-state data for the MCMC run.
+# Prepare 4-state data for the MCMC runs.
 #
 # Inputs:
 #	calibration_data.rds
 
 # outputs:
-#	transitions.txt  (transition dataset for the mcmc)
+#	dat/mcmc_calib.txt  (transition dataset for the mcmc)
 
 
+# arguments
+# -r5: uses a basal area cutoff of 5 for state R
+# -ifile=filename.rds: rds file containing initial values to use; if missing set to 0
 
-calib = readRDS("dat/calibration_data.rds")
-inits = readRDS("dat/inits.rds")
+arg = commandArgs(trailingOnly = TRUE)
+
+r.level = 1
+if('-r5' %in% arg) r.level = 5
+
+# handle initial values
+ifile = "dat/inits_zero.rds"
+if(length(iarg <- grep('-i=', arg, value=T)) > 0)
+	ifile = substr(iarg, 4, nchar(iarg))
+
+initialVals = readRDS(ifile)
+
+calib.name = paste0("dat/calibration_data_r", r.level, ".rds")
+calib.output.name = paste0("dat/mcmc_calib_r", r.level, ".txt")
+calib = readRDS(calib.name)
+
 
 # set up the transition file:
 mcmcData = data.frame(
@@ -29,16 +48,17 @@ mcmcData = data.frame(
 	prevalenceR = calib[,"R"]
 )
 
-write.csv(mcmcData, "dat/mcmc_calib.txt", row.names=FALSE)
+write.csv(mcmcData, calib.output.name, row.names=FALSE)
 
 
-# prep the inits
+# prep the inits - full model
+inits = initialVals$full
 parNames = colnames(inits)
 isConstant = rep(0,length(parNames))
 isConstant[grep("[a-z][a-z]?[5-6]", parNames)] = 1
 for(i in 1:nrow(inits))
 {
-	mcmcIFile = paste0("dat/mcmc_inits", i, ".txt")
+	mcmcIFile = paste0("dat/mcmcInits_full_r", r.level, "_", i, ".txt")
 	mcmcInits = data.frame(
 		name = parNames,
 		initialValue = as.numeric(inits[i,]),
@@ -51,17 +71,42 @@ for(i in 1:nrow(inits))
 	write.csv(mcmcInits, mcmcIFile, row.names=FALSE)
 }
 
+
+# prep the ab constant inits
+isConstant = rep(0,length(parNames))
+isConstant[grep("[a-z][a-z]?[5-6]", parNames)] = 1
+isConstant[grep("ab[1-4]", parNames)] = 1
+inits = initialVals$ab
+for(i in 1:nrow(inits))
+{
+	mcmcIFile = paste0("dat/mcmcInits_ab_r", r.level, "_", i, ".txt")
+	mcmcInits = data.frame(
+		name = parNames,
+		initialValue = as.numeric(inits[i,]),
+		samplerVariance = 0.5,
+		priorMean = 0,
+		priorSD = 2.5,
+		priorDist = "Cauchy",
+		isConstant = isConstant)
+	mcmcInits$priorSD[grep("[a-z][a-z]?[0]", parNames)] = 10
+	write.csv(mcmcInits, mcmcIFile, row.names=FALSE)
+}
+
+
 # prep the intercept only inits
 isConstant = rep(1,length(parNames))
 isConstant[grep("[a-z][a-z]?[0]", parNames)] = 0
-mcmcIFile = "dat/mcmc_inits_int.txt"
-mcmcInits = data.frame(
-	name = parNames,
-	initialValue = 0,
-	samplerVariance = 0.5,
-	priorMean = 0,
-	priorSD = 2.5,
-	priorDist = "Cauchy",
-	isConstant = isConstant)
-mcmcInits$priorSD[grep("[a-z][a-z]?[0]", parNames)] = 10
-write.csv(mcmcInits, mcmcIFile, row.names=FALSE)
+inits = initialVals$int
+for(i in 1:nrow(inits)) {
+	mcmcIFile = paste0("dat/mcmcInits_int_r", r.level, "_", i, ".txt")
+	mcmcInits = data.frame(
+		name = parNames,
+		initialValue = 0,
+		samplerVariance = 0.5,
+		priorMean = 0,
+		priorSD = 2.5,
+		priorDist = "Cauchy",
+		isConstant = isConstant)
+	mcmcInits$priorSD[grep("[a-z][a-z]?[0]", parNames)] = 10
+	write.csv(mcmcInits, mcmcIFile, row.names=FALSE)
+}
